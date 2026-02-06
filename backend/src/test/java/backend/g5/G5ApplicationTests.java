eac6952bf991eef5f7658cd3c293b991c9c154a7
Van Kehrby Lubanga <kehrbymartinez@gmail.com>
2026-02-06 12:04:27 +0800

    Transferred all files to a new repo

---
+++ backend/src/test/java/backend/g5/G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.g5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
